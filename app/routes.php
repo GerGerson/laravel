34103ca3ecbc8b625d4f8c3c25dc1e310cf77c46
GerGerson <jasonlai11281983@gmail.com>
2013-07-24 17:45:01 +0800

    Test

--- app/routes.php
+++ app/routes.php
@@ -19,6 +19,7 @@ Route::post('/Temp/{id}', 'HomeController@getWebDIV');
 Route::post('/Module/{id}', 'HomeController@getModuleCode');
 Route::post('/Like/Promo/{type}', 'HomeController@getLike');
 
+<<<<<<< HEAD
 Route::get('/LikeSetting/{t_id}', 'LikeSetting@getDivSetting');
 
 //jason
@@ -26,3 +27,8 @@ Route::get('/', function()
 {
     return 'Hello World';
 });
+=======
+//Route::post('/Like/Promor/{type}', 'HomeController@getLike');
+//Testing ABC
+
+>>>>>>> 25411caf4d9eafd815dbb2a746a38ec6dd2ec368
